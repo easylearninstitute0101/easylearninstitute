f51ceb234c727622a2e8e6bfa73b88da3936741c
easylearninstitute0101 <makaiyum973@gmail.com>
2026-09-14 01:06:32 +0600

    Bump PWA cache to v3 after production fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'easylearn-v2';
+const CACHE = 'easylearn-v3';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', event => {
